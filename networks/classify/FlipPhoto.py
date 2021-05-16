0e41b249ebd36605ea466e8f916b428b22570e9f
qing-xue <964262601@qq.com>
2021-05-16 21:14:02 +0800

    FlipPhoto: add filter_list

--- networks/classify/FlipPhoto.py
+++ networks/classify/FlipPhoto.py
@@ -27,30 +27,29 @@ def rotate_img(degree, img_name, path_img):
     new_img = cv2.warpAffine(img, matRotate, (height, width))
 
     path_new_img = path_img.replace(img_name, "") + new_img_name
-    # print(path_new_img)
+    print(path_new_img)
     cv2.imwrite(path_new_img, new_img)
 
 
 def main_func(data_dir):
-    count = 0
     for root, dirs, _ in os.walk(data_dir):
         # 遍历类别
         for sub_dir in dirs:
             img_names = os.listdir(os.path.join(root, sub_dir))
-            img_names = list(filter(lambda x: x.endswith('.bmp'), img_names))
+            # img_names = list(filter(lambda x: x.endswith('.bmp'), img_names))  # 这一步耗时？
 
             # 遍历图片
             for i in range(len(img_names)):
-                print(count)
-                count = count + 1
                 img_name = img_names[i]
+                filter_list = ['rotate', 'flip']  # 避免重复增强图块
+                if not img_name.endswith('.bmp') or any(key in img_name for key in filter_list):
+                    continue
+
                 path_img = os.path.join(root, sub_dir, img_name)
                 grade = sub_dir
-                # print(img_name, '\n', path_img, '\n', grade)
-                # print(sub_dir)
 
                 # 分类 0 只需要翻转
-                if grade == "L0":
+                if 'L0' == grade:
                     img = cv2.imread(path_img)
                     horizontal_img = cv2.flip(img, 1)
                     new_img_name = img_name.replace(".bmp", "") + "-flip" + ".bmp"
@@ -59,12 +58,14 @@ def main_func(data_dir):
                     cv2.imwrite(path_new_img, horizontal_img)
 
                 # 分类 2 翻转后 + rotate1.5 + rotate-1.5
-                if grade == "L2":
+                elif 'L2' == grade:
                     img = cv2.imread(path_img)
                     horizontal_img = cv2.flip(img, 1)
                     new_img_name = img_name.replace(".bmp", "") + "-flip" + ".bmp"
                     path_new_img = os.path.join(root, sub_dir, new_img_name)
+                    print(path_new_img)
                     cv2.imwrite(path_new_img, horizontal_img)
+
                     rotate_img(-1.5, img_name, path_img)
                     rotate_img(1.5, img_name, path_img)
                     rotate_img(-1.5, new_img_name, path_new_img)
@@ -74,5 +75,5 @@ def main_func(data_dir):
 if __name__ == '__main__':
     # 'D:\BaiduNetdiskDownload\Proenviroment\Heshanimgset\Heshanimgset\Results\split1'
     img_folder = sys.argv[1]
-    print('该目录下需包含 3类 子文件（L0/L1/L2）：', img_folder)  # 命令行传入文件路径
+    print('该目录下需包含 3类 子文件（L0/L1/L2），可以嵌套：', img_folder)  # 命令行传入文件路径
     main_func(img_folder)
